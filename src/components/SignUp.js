bd195cf1dd15b911cf6ecbedcadfcad70cc3444b
Ryan P <rdpinkstaff@gmail.com>
2022-06-09 18:53:56 -0600

    Worked on sign up validation

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 import "./SignUp.css";
 
 function SignUp() {
+  const [newEmail, setNewEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const checkPasswords = (set, confirm) => {
+    return set === confirm;
+  };
+
+  const register = async (e) => {
+    e.preventDefault();
+    if (newEmail !== "" && checkPasswords(password, confirmPassword)) {
+      createUserWithEmailAndPassword(auth, newEmail, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode + ": " + errorMessage);
+        });
+    } else return;
+  };
+
   return (
     <div className="sign-up">
-        <form className="create-user">
+      <form className="create-user" onSubmit={register}>
         <h1 className="title-form">El Instagram Clone</h1>
-        <p className="description-site">Sign up to see photos from friends and family.</p>
+        <p className="description-site">
+          Sign up to see photos from friends and family.
+        </p>
         <input
-            className="sign-form"
-            type="email"
-            placeholder="Email address"
-          />
-          <input className="sign-form" type="password" placeholder="Password" />
-          <input className="sign-form" type="password" placeholder="Confirm Password" />
-          <button className="btn-signup" type="submit">
-            Sign up
-          </button>
-        </form>
+          className="sign-form"
+          type="email"
+          placeholder="Email address"
+          onChange={(e) => setNewEmail(e.target.value)}
+        />
+        <input
+          className="sign-form"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          className="sign-form"
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button className="btn-signup" type="submit">
+          Sign up
+        </button>
+      </form>
       <div className="account-have">
         <p className="center-txt">
           Have an account? <Link to="/">Log in</Link>
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD3ObaBVa1UqRRXr47KdzivxrOsFIY9qaU",
   authDomain: "insta-clone-f3771.firebaseapp.com",
   projectId: "insta-clone-f3771",
   storageBucket: "insta-clone-f3771.appspot.com",
   messagingSenderId: "239707590758",
-  appId: "1:239707590758:web:b6af43c8b2129ed8821da7"
+  appId: "1:239707590758:web:b6af43c8b2129ed8821da7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
