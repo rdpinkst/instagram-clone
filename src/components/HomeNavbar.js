ae475e89921a04e0eafad7e1d1f8291b3fa56832
Ryan P <rdpinkstaff@gmail.com>
2022-06-22 07:28:17 -0600

    Basic navbar added to home

---
+++ src/components/HomeNavbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HomeNavbar.css"
+
+function HomeNavbar(){
+    return (
+        <nav>
+            <h1 className="site-title">El-Instagram Clone</h1>
+            <ul className="profile-links">
+                <li>Add pic</li>
+                <li>Profile</li>
+                <li>Log Out</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default HomeNavbar;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
-import { Navigate } from "react-router-dom"
+import { Navigate } from "react-router-dom";
+import HomeNavbar from "./HomeNavbar";
+import "./Homepage.css";
 
-function Homepage(){
-    const [signedOut, setSignedOut] = useState(false);
+function Homepage() {
+  const [signedOut, setSignedOut] = useState(false);
 
-    const logOut = async() => {
-        signOut(auth).then(() => {
-            setSignedOut(true);
-            console.log("Logged Out")
-        }).catch((error) => {
-            console.log(error.message)
-        })
-    }
+  const logOut = async () => {
+    signOut(auth)
+      .then(() => {
+        setSignedOut(true);
+        console.log("Logged Out");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-    return (
-        <div>
-            {signedOut && <Navigate to="/" replace={true} />}
-            <h1>Logged in</h1>
-            <button onClick={logOut}>Sign Out</button>
-        </div>
-    )
+  return (
+    <div className="page">
+      <HomeNavbar />
+      {signedOut && <Navigate to="/" replace={true} />}
+      <div className="content">
+        <h1>Logged in</h1>
+        <button onClick={logOut}>Sign Out</button>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
