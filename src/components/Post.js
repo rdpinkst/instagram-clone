a6106c4d83a91427d8f9daea79a33271a9d046e8
Ryan P <rdpinkstaff@gmail.com>
2022-06-28 18:55:31 -0600

    Styled and got svg for like and comment

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,23 +1,34 @@
 import React from "react";
+import Icon from "@mdi/react";
+import { mdiHeartOutline, mdiMessageOutline } from "@mdi/js";
 
-function Post( {postData} ) {
-
-    const comments = postData.comments.map(comment => {
-        return (
-            <div key={comment.uid}>
-                <p>{comment.userName}</p>
-                <p>{comment.message}</p>
-            </div>
-        )
-    })
-
+function Post({ postData }) {
+  const comments = postData.comments.map((comment) => {
     return (
-        <div className="postContainer">
-            <img className="postImage" src={`/${postData.picUrl}`} alt="spanish learning" />
-            <p>{postData.caption}</p>
-            {comments}
-        </div>
-    )
+      <div key={comment.uid} className="comment">
+        <div className="profile-pic"></div>
+        <p className="user">{comment.userName}</p>
+        <p>{comment.message}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className="postContainer">
+      <img
+        className="postImage"
+        src={`/${postData.picUrl}`}
+        alt="spanish learning"
+      />
+      <div className="like-comment">
+        <Icon className="icon" path={mdiHeartOutline} size={1} />
+        <Icon className="icon" path={mdiMessageOutline} size={1} />
+      </div>
+
+      <p className="description-post">{postData.caption}</p>
+      {comments}
+    </div>
+  );
 }
 
 export default Post;
