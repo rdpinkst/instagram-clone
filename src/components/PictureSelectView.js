5df972c298dc0a4441b84812878de0dfa9fa55ed
Ryan P <rdpinkstaff@gmail.com>
2022-07-21 18:18:37 -0600

    Somewhat completed picture selected view

--- src/components/PictureSelectView.js
+++ src/components/PictureSelectView.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Icon from "@mdi/react";
+import { mdiHeartOutline, mdiMessageOutline } from "@mdi/js";
 
 function PictureSelectView({ picUrl, setPicUrl }) {
-  function clearPicUrl(){
+  function clearPicUrl() {
     setPicUrl("");
   }
   return (
@@ -9,7 +11,21 @@ function PictureSelectView({ picUrl, setPicUrl }) {
       <div className="picture-container">
         <img className="selected-img" src={picUrl} alt="random pic" />
       </div>
-      <span className="close-card" onClick={clearPicUrl}>x</span>
+      <div className="content-pic">
+        <span className="close-card" onClick={clearPicUrl}>
+          x
+        </span>
+        <div className="like-comment card-comment">
+          <Icon className="icon" path={mdiHeartOutline} size={1.1} />
+          <Icon className="icon" path={mdiMessageOutline} size={1.1} />
+        </div>
+        <p className="bold-text">This is the information about the picture</p>
+        <div className="comment">
+          <div className="profile-pic"></div>
+          <p className="user">Users Name</p>
+          <p>This is a random comment</p>
+        </div>
+      </div>
     </div>
   );
 }
