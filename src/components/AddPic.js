e78527656a4c36298bb649e62631982740e6bc41
Ryan P <rdpinkstaff@gmail.com>
2022-07-23 10:43:40 -0600

    Figured out file object and getting url to display preview

--- src/components/AddPic.js
+++ src/components/AddPic.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function AddPic(){
-    return (
-        <p>Add Pic to data</p>
-    )
+function AddPic() {
+  const [picUpload, setPicUpload] = useState("")
+
+  function getPicUrl(e){
+    const filePic = e.target.files[0];
+    const picUrl = URL.createObjectURL(filePic);
+    setPicUpload(picUrl);
+  }
+
+  return (
+    <div>
+      <input onChange={getPicUrl}type="file" />
+      <div className="preview">
+        <img className="selected-img" src={picUpload} alt="pic to upload"/>
+      </div>
+    </div>
+  );
 }
 
 export default AddPic;
