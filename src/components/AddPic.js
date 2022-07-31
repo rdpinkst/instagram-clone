ae343838eeae31875b6164c6ee84cd8838e3004d
Ryan P <rdpinkstaff@gmail.com>
2022-07-31 17:54:53 -0600

    Minor change to button

--- src/components/AddPic.js
+++ src/components/AddPic.js
@@ -1,38 +1,52 @@
-import React, {useState} from "react";
-import {Navigate} from "react-router-dom";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 import Icon from "@mdi/react";
-import { mdiUpload } from '@mdi/js'; 
+import { mdiUpload } from "@mdi/js";
 
-function AddPic({user}) {
-  const [picUpload, setPicUpload] = useState("")
+function AddPic({ user }) {
+  const [picUpload, setPicUpload] = useState("");
 
-  function getPicUrl(e){
+  function getPicUrl(e) {
     const filePic = e.target.files[0];
     const picUrl = URL.createObjectURL(filePic);
     setPicUpload(picUrl);
   }
 
-  if(user){
-  return (
-    <div>
-      {!picUpload && <div className="preview border" onClick={() => document.getElementById("get-pic").click()}>
-        <Icon path={mdiUpload} size={10} />
-        <input id="get-pic" onChange={getPicUrl} type="file" style={{display: "none"}}/>
-      </div>}
-      
-      {picUpload && <div className="preview">
-        <img className="selected-img" src={picUpload} alt="pic to upload"/>
-        <form className="form-picdesc">
-          <label for="description" className="desc-bold">Description: </label>
-          <textarea id="description" type="text" /> 
-        </form>
-        <button className="btn-signup full-width">Submit</button>
-      </div>}
-    </div>
-  );
+  if (user) {
+    return (
+      <div>
+        {!picUpload && (
+          <div
+            className="preview border"
+            onClick={() => document.getElementById("get-pic").click()}
+          >
+            <Icon path={mdiUpload} size={10} />
+            <input
+              id="get-pic"
+              onChange={getPicUrl}
+              type="file"
+              style={{ display: "none" }}
+            />
+          </div>
+        )}
+
+        {picUpload && (
+          <div className="preview">
+            <img className="selected-img" src={picUpload} alt="pic to upload" />
+            <form className="form-picdesc">
+              <label htmlFor="description" className="desc-bold">
+                Description:{" "}
+              </label>
+              <textarea id="description" type="text" />
+            </form>
+            <button className="btn-signup full-width">Submit</button>
+          </div>
+        )}
+      </div>
+    );
   }
 
-  if(!user){
+  if (!user) {
     return <Navigate to="/" replace={true} />;
   }
 }
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Comment(){
-    return (
-        <form className="comment-container">
-            <textarea className="comment-description" />
-            <button className="log-in" style={{marginTop: "0"}}>Submit</button>
-        </form>
-    )
+function Comment() {
+  const [comment, setComment] = useState("");
+
+  return (
+    <form className="comment-container">
+      <textarea
+        className="comment-description"
+        onChange={(e) => setComment(e.target.value)}
+      />
+      {comment && (
+        <button className="log-in" style={{ marginTop: "0" }}>
+          Submit
+        </button>
+      )}
+    </form>
+  );
 }
 
 export default Comment;
